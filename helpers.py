057e1fbb7c12306e9d0de7ae2a3abad72911b99a
kunnapool <kpool@uvic.ca>
2021-08-11 15:36:07 -0700

    resize images with BB

--- helpers.py
+++ helpers.py
@@ -67,19 +67,26 @@ def nonmax_sup(b1, b2, b1_p, b2_p, is_midpt=False, iou_threshold=0.5):
 
 
 # https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
-def image_resize_same_aspect_ratio(image, width = -1, height = -1):
-    new_size = 0
+def resize_image_with_BB(image, width, height, BB):
     h = image.shape[0]
     w = image.shape[1]
 
-    if width == -1:
-        ratio = height / float(h)
-        new_size = (int(w * ratio), height)
-    else:
-        ratio = width / float(w)
-        new_size = (width, int(h * ratio))
+    new_size = (width, height)
 
-    return cv2.resize(image, new_size, interpolation = cv2.INTER_AREA)
+    x1, y1, x2, y2 = BB
+
+    r_w = width/w
+    r_h = height/h
+
+    x1 *= r_w
+    x2 *= r_w
+
+    y1 *= r_h
+    y2 *= r_h
+
+    new_BB = (int(x1), int(y1), int(x2), int(y2))
+
+    return cv2.resize(image, new_size, interpolation = cv2.INTER_AREA), new_BB
 
 """
 References:
